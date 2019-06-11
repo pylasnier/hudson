dd35d207715eabaaa9a1455cfa671ceced96a66d
Pascal Lasnier <py@lasnier.com>
2019-06-12 00:04:47 +0100

    Home: Added basic player and camera movement with a playzone

--- Hudson Game/Game1.cs
+++ Hudson Game/Game1.cs
@@ -1,33 +1,79 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
+using static Hudson_Game.GameObjects;
 
 namespace Hudson_Game
 {
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+        Texture2D _playZoneTexture;
+
+        private Level _level;
+        private Camera _camera;
+        private Texture2D _playerTexture;
+        private Player _player;
+        private bool _keyDownRecorded;
+
+        public event EventHandler ContentLoaded;
 
         public Game1()
         {
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            _graphics.PreferredBackBufferWidth = 1024;
+            _graphics.PreferredBackBufferHeight = 768;
+            _graphics.ApplyChanges();
+            
+            ContentLoaded += UseContent;
+        }
+
+        private void UseContent(object sender, EventArgs e)
+        {
+            _level = new Level(_playZoneTexture, new Rectangle(100, 100, 800, 800));
+            _player = new Player(_playerTexture, _playerTexture, _playerTexture, _playerTexture, 1, 1, new Vector2(400, 400));
+            _camera.Target = _player;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _camera = new Camera(new Vector2(400, 400), null);
+            _keyDownRecorded = false;
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _playZoneTexture = Content.Load<Texture2D>("playzone");
+            
+            _playerTexture = new Texture2D(GraphicsDevice, 80, 80);
+            
+            var data = new Color[80 * 80];
+            for (int i = 0; i < 80; i++)
+            {
+                for (int j = 0; j < 80; j++)
+                {
+                    if ((i - 40) * (i - 40) + (j - 40) * (j - 40) <= 40 * 40)
+                        data[80 * i + j] = Color.Blue;
+                    else
+                        data[80 * i + j] = Color.Transparent;
+                }
+            }
+            
+            _playerTexture.SetData(data);
+
+            OnContentLoaded();
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,6 +83,47 @@ namespace Hudson_Game
                 Exit();
 
             // TODO: Add your update logic here
+            var moveVector = Vector2.Zero;
+            
+            if (Keyboard.GetState().IsKeyDown(Keys.W)) moveVector += -Vector2.UnitY;
+            if (Keyboard.GetState().IsKeyDown(Keys.S)) moveVector += Vector2.UnitY;
+            if (Keyboard.GetState().IsKeyDown(Keys.A)) moveVector += -Vector2.UnitX;
+            if (Keyboard.GetState().IsKeyDown(Keys.D)) moveVector += Vector2.UnitX;
+
+            if (!_keyDownRecorded && Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                _keyDownRecorded = true;
+                if (_camera.CameraState == CameraState.Free)
+                    _camera.CameraState = CameraState.Locked;
+                else
+                    _camera.CameraState = CameraState.Free;
+            }
+            else if (_keyDownRecorded && Keyboard.GetState().IsKeyUp(Keys.Enter))
+            {
+                _keyDownRecorded = false;
+            }
+
+            if (moveVector != Vector2.Zero)
+            {
+                switch (_camera.CameraState)
+                {
+                    case CameraState.Free:
+                        _camera.Move(Vector2.Normalize(moveVector) * 100f, gameTime);
+                        break;
+                    
+                    case CameraState.Locked:
+                        _player.Move(Vector2.Normalize(moveVector) * 100f, gameTime);
+                        break;
+                    
+                    case CameraState.Chase:
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            
+            _camera.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -46,8 +133,23 @@ namespace Hudson_Game
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_level.Texture,
+                new Vector2(_graphics.PreferredBackBufferWidth / 2f - _camera.Position.X - _level.PlayZone.X,
+                    _graphics.PreferredBackBufferHeight / 2f - _camera.Position.Y - _level.PlayZone.Y));
+            _spriteBatch.Draw(_player.Standing,
+                new Vector2(_graphics.PreferredBackBufferWidth / 2f + _player.Position.X - _camera.Position.X,
+                    _graphics.PreferredBackBufferHeight / 2f + _player.Position.Y - _camera.Position.Y), null,
+                Color.White, 0f, new Vector2(_player.Standing.Width / 2f, _player.Standing.Height / 2f), 1f,
+                SpriteEffects.None, 0);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        protected virtual void OnContentLoaded()
+        {
+            ContentLoaded?.Invoke(this, EventArgs.Empty);
         }
     }
 }
---
+++ Hudson Game/GameObjects.cs
@@ -0,0 +1,124 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Hudson_Game
+{
+    public static class GameObjects
+    {
+        public struct Level
+        {
+            public Texture2D Texture { get; }
+            public Rectangle PlayZone { get; }
+
+            public Level(Texture2D texture, Rectangle playZone)
+            {
+                Texture = texture;
+                PlayZone = playZone;
+            }
+        }
+
+        public struct EnvironmentObject
+        {
+            public Texture2D Texture { get; }
+            public Vector2 Position { get; }
+            public Vector2 Scale { get; }
+            public bool CollisionEnabled { get; }
+
+            public EnvironmentObject(Texture2D texture, Vector2 position, Vector2 scale, bool collisionEnabled)
+            {
+                Texture = texture;
+                Position = position;
+                Scale = scale;
+                CollisionEnabled = collisionEnabled;
+            }
+        }
+
+        public class Player
+        {
+            public Texture2D Standing { get; }
+            public Texture2D Starting { get; }
+            public Texture2D Running { get; }
+            public Texture2D Stopping { get; }
+
+            private int _startingFrames;
+            private int _runningFrames;
+
+            public Vector2 Position { get; private set; }
+            public Vector2 Direction { get; private set; }
+
+            public PlayerState PlayerState { get; private set; }
+            public int Frame { get; private set; }
+
+            public Player(Texture2D standing, Texture2D starting, Texture2D running, Texture2D stopping, int startingFrames, int runningFrames, Vector2 position)
+            {
+                Standing = standing;
+                Starting = starting;
+                Running = running;
+                Stopping = stopping;
+                _startingFrames = startingFrames;
+                _runningFrames = runningFrames;
+                Position = position;
+
+                PlayerState = PlayerState.Standing;
+                Direction = -Vector2.UnitY;
+                Frame = 0;
+            }
+
+            public void Move(Vector2 moveVector, GameTime gameTime)
+            {
+                Position += moveVector * (float) gameTime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+
+        public class Camera
+        {
+            public Vector2 Position { get; set; }
+            public CameraState CameraState { get; set; }
+            public Player Target { get; set; }
+
+            public Camera()
+            {
+                Position = Vector2.Zero;
+                CameraState = CameraState.Free;
+            }
+
+            public Camera(Vector2 position, Player target)
+            {
+                Position = position;
+                Target = target;
+                CameraState = CameraState.Locked;
+            }
+
+            public void Move(Vector2 moveVector, GameTime gameTime)
+            {
+                if (CameraState == CameraState.Free)
+                {
+                    Position += moveVector * (float) gameTime.ElapsedGameTime.TotalSeconds;
+                }
+            }
+
+            public void Update(GameTime gameTime)
+            {
+                if (Target != null && CameraState == CameraState.Locked)
+                {
+                    Position = Target.Position;
+                }
+            }
+        }
+
+        public enum PlayerState
+        {
+            Standing = 0,
+            Starting = 1,
+            Running = 2,
+            Stopping = 3
+        }
+
+        public enum CameraState
+        {
+            Free = 0,
+            Locked = 1,
+            Chase = 2
+        }
+    }
+}
